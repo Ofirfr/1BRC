b65f96e27b00d58aa00026a9dbeff80a7439f2b7
Ofirfr <ofir.frisch1@gmail.com>
2024-05-11 12:59:13 +0300

    change to 10000 unique cities

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # 1BRC
+
 My first time in Golang!
 
-Current record for 1 Million rows is ~350ms
+Current record for 1 Million rows is ~400ms
 Goal for 1 Million rows currently is 10ms
 Lets go!
--- data/generate_sample_data.py
+++ data/generate_sample_data.py
@@ -12,16 +12,19 @@ with open("./data/worldcities.csv", newline='', encoding="utf-8") as csvfile:
 random.shuffle(city_names)
 
 # Generate 999 different stations with real city names
-stations = city_names[:999]
+stations = city_names[:10000]
 
 # Function to generate a random temperature measurement with one fractional digit
+
+
 def generate_measurement():
     return round(random.uniform(-99.9, 99.9), 1)
 
+
 # Generate temperature measurements for each station
 with open("./data/temperature_data.txt", "w", encoding="utf-8") as file:
-  row_count = 1000000 # Million!
-  for _ in range (row_count):
-       station = random.choice(stations)
-       measurement = generate_measurement()
-       file.write(f"{station};{measurement}\n")
+    row_count = 1000000  # Million!
+    for _ in range(row_count):
+        station = random.choice(stations)
+        measurement = generate_measurement()
+        file.write(f"{station};{measurement}\n")
