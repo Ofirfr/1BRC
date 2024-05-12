e281e1c4732ad877b2dcc9ba931bb8cf432c482a
Ofirfr <ofir.frisch1@gmail.com>
2024-05-12 18:24:04 +0300

    add buffer to channels

--- src/playground/solution.go
+++ src/playground/solution.go
@@ -5,67 +5,127 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
 )
 
-type CityStatistics struct {
-	Max   float64
-	Min   float64
-	Sum   float64
-	Count float64
+const Max = 1
+const Min = 2
+const Sum = 3
+const Count = 4
+const NumOfParsers = 1
+const NumOfAggregators = 1
+const ChannelBuffer = 100000
+
+func CalculateStatistics() map[string]*structs.CityResult {
+	var wgParser sync.WaitGroup
+	var wgAggregator sync.WaitGroup
+
+	lines := make(chan string, ChannelBuffer)
+	dataPoints := make(chan DataPoint, ChannelBuffer)
+	result := make(chan map[string]*structs.CityResult, ChannelBuffer)
+	go producer(lines)
+	for i := 0; i < NumOfParsers; i++ {
+		wgParser.Add(1)
+		go func() {
+			defer wgParser.Done()
+			log.Println("Starting parser")
+			parser(lines, dataPoints)
+		}()
+	}
+	for i := 0; i < NumOfAggregators; i++ {
+		wgAggregator.Add(1)
+		go func() {
+			defer wgAggregator.Done()
+			log.Println("Starting aggregator")
+			aggregator(dataPoints, result)
+		}()
+	}
+	go func() {
+		wgParser.Wait()
+		close(dataPoints)
+	}()
+	go func() {
+		wgAggregator.Wait()
+		close(result)
+	}()
+
+	return <-result
 }
 
-func CalculateStatistics() map[string]structs.CityResult {
+func producer(c chan string) {
+	defer close(c)
 	file, err := os.Open("../data/temperature_data.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	statistics := make(map[string]*CityStatistics)
-
 	scanner := bufio.NewScanner(file)
-	lines := make([]string, 10)
 	for scanner.Scan() {
 		line := scanner.Text()
-		lines = append(lines, line)
+		c <- line
 	}
-	// 	// Parse the line into city and temperature
-	// 	city_and_temp := strings.Split(line, ";")
-	// 	city := city_and_temp[0]
-	// 	temp_string := city_and_temp[1]
-	// 	temp, err := strconv.ParseFloat(temp_string, 64)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
 
-	// 	cityStats, ok := statistics[city]
-	// 	if !ok {
-	// 		statistics[city] = &CityStatistics{
-	// 			Max:   -100,
-	// 			Min:   100,
-	// 			Sum:   0,
-	// 			Count: 0,
-	// 		}
-	// 		cityStats = statistics[city]
-	// 	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
 
-	// 	cityStats.Max = max(cityStats.Max, temp)
-	// 	cityStats.Min = min(cityStats.Min, temp)
-	// 	cityStats.Sum = cityStats.Sum + temp
-	// 	cityStats.Count = cityStats.Count + 1
+type DataPoint struct {
+	city string
+	temp float64
+}
 
-	// }
-
-	// if err := scanner.Err(); err != nil {
-	// 	log.Fatal(err)
-	// }
-	results := make(map[string]structs.CityResult, 10000)
-	for city, cityStats := range statistics {
-		results[city] = structs.CityResult{
-			Max:     cityStats.Max,
-			Min:     cityStats.Min,
-			Average: cityStats.Sum / cityStats.Count,
+func parser(c chan string, d chan DataPoint) {
+	for line := range c {
+		// Parse the line into city and temperature
+		city_and_temp := strings.Split(line, ";")
+		city := city_and_temp[0]
+		temp_string := city_and_temp[1]
+		temp, err := strconv.ParseFloat(temp_string, 32)
+		if err != nil {
+			log.Fatal(err)
 		}
+		d <- DataPoint{city, temp}
 	}
-	return results
+}
+
+func aggregator(d chan DataPoint, r chan map[string]*structs.CityResult) {
+	statistics := make(map[string]*structs.CityResult, 10000)
+
+	for dataPoint := range d {
+		city, temp := dataPoint.city, dataPoint.temp
+		cityResult, ok := statistics[city]
+		if !ok {
+			emptyCityResult := &structs.CityResult{
+				Max:     -100,
+				Min:     100,
+				Sum:     0,
+				Count:   0,
+				Average: 0,
+			}
+			statistics[city] = emptyCityResult
+			cityResult = emptyCityResult
+		}
+		cityResult.Max = max(cityResult.Max, temp)
+		cityResult.Min = min(cityResult.Min, temp)
+		cityResult.Sum = cityResult.Sum + temp
+		cityResult.Count = cityResult.Sum + 1
+	}
+
+	r <- statistics
+}
+func measureTime(f func() map[string]*structs.CityResult) time.Duration {
+	startTime := time.Now()
+
+	f()
+
+	endTime := time.Now()
+
+	duration := endTime.Sub(startTime)
+
+	return duration
 }
